f54699d46b9bcba393655d9b0f8cfdf04a471473
Mike Hodgson <mhodgson@gmail.com>
2018-04-26 08:01:27 -0400

    Initial commit.

---
+++ app/src/main/java/com/mikehodgson/tsmt/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mikehodgson.tsmt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
