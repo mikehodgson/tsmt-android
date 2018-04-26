0c6da1e11ef583e7986427e9f5e38e28fed64e07
Mike Hodgson <mhodgson@gmail.com>
2018-04-26 08:28:28 -0400

    Remove submodule, just pull in the web app.

--- app/src/main/java/com/mikehodgson/tsmt/MainActivity.java
+++ app/src/main/java/com/mikehodgson/tsmt/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mikehodgson.tsmt;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -9,17 +11,31 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
 
+    private class MyCustomWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if(url.contains("https://homehacks.ca")) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+            } else {
+                view.loadUrl(url);
+            }
+            return true;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new MyCustomWebViewClient());
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
 
-        mWebView.loadUrl("file:///android_asset/www/index.html");
+        mWebView.loadUrl("https://homehacks.ca/tsmt/index.html");
     }
 }
